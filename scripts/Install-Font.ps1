d11a0559121a6241d76b8a3cb2e3a4ea0a1ba32d
Jarmos-san <somraj.mle@gmail.com>
2021-05-27 18:45:20 +0530

    :recycle: Reformt code

--- scripts/Install-Font.ps1
+++ scripts/Install-Font.ps1
@@ -9,6 +9,10 @@ https://poshcode.gitbook.io/powershell-practice-and-style/style-guide/readabilit
 * Nerd Fonts Homepage: https://www.nerdfonts.com/font-downloads
 
 * Installing the fonts: https://powers-hell.com/2020/06/09/installing-fonts-with-powershell-intune/
+* https://superuser.com/questions/813039/what-is-the-purpose-of-the-fonts-key-in-the-registry
+* https://gist.github.com/anthonyeden/0088b07de8951403a643a8485af2709b
+* https://powers-hell.com/2020/06/09/installing-fonts-with-powershell-intune/
+* https://stackoverflow.com/questions/24987542/is-there-a-link-to-github-for-downloading-a-file-in-the-latest-release-of-a-repo/54836319#54836319
 
 Specific to this script, it downloads the FiraCode Nerd Font available at: https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip.
 For now it's URI is hardcoded but it might be advisable to dynamically download the latest version as & when required.
@@ -30,15 +34,16 @@ TODO:
     ? Use variables & Environment variables instead of hard coding. For e.g the $output variable.
     ? Refactor code into proper functions & what not. It should confer to good coding practices & best practices.
 #>
+# Following URL might be more apt:
+# https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf
+
+$PatchedFont = "FiraCode"
+$Uri = "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$PatchedFont.zip"
+$OutputFilename = "$ENV:USERPROFILE\Downloads\$PatchedFont.zip"
 
 # Downloads the zipped file
-$url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
-$output = "C:\Users\Space\Downloads\FiraCode.zip"
-
-# Downloads the zipped file
-Write-Host "Downloading the zipped file..."
-(New-Object System.Net.WebClient).DownloadFile($url, $output)
-Write-Host "Done..."
+Write-Host "Downloading the zipped file..." -ForegroundColor Magenta
+Invoke-WebRequest -Uri $Uri -OutFile $OutputFilename
 
 # Extract & then deletes the zipped file
 Write-Host "Extracting the zipped file..."
