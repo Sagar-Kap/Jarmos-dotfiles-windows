8179df57683b229e8dbfcae2d50526bfbe3d72d0
Jarmos-san <somraj.mle@gmail.com>
2021-05-28 15:46:41 +0530

    :truck: Backup config files to GitHub

--- configurations/windows-powershell/Microsoft.PowerShell_profile.ps1
+++ configurations/windows-powershell/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,6 @@ Invoke-Expression (&starship init powershell)
 
 # Configures Starship to use the %TEMP% directory for caching
 $ENV:STARSHIP_CACHE = Resolve-Path -Path $ENV:TEMP
+
+# Path to the Starship configuration file
+$ENV:STARSHIP_CONFIG = Resolve-Path -Path "$ENV:USERPROFILE\.starship\starship.toml
