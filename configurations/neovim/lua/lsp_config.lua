73367ecfd400276f1b56fd506f6d41c056c3834d
Jarmos-san <somraj.mle@gmail.com>
2021-07-16 15:45:03 +0530

    :truck: Backup config files to GitHub

--- configurations/neovim/lua/lsp_config.lua
+++ configurations/neovim/lua/lsp_config.lua
@@ -1,3 +1,27 @@
 local lspconfig = require('lspconfig')
 
-lspconfig.pyright.setup{}                               -- Pyright Language Server for Python files
+-- Pyright Language Server for Python files
+lspconfig.pyright.setup{}
+
+-- TypeScript and/or JavaScript language server
+lspconfig.tsserver.setup{}                              
+
+-- JSON language server --
+-- TODO: Configure it further based on info available in the documentations
+lspconfig.jsonls.setup{
+    commands = {
+        Format = {
+            function()
+                vim.lsp.buf.range_formatting({}, {0,0}, {vim.fn.line("$"), 0})
+            end
+        }
+    }
+}
+
+-- HTML/CSS language server --
+-- TODO: Configure & setup snippet plugin like LuaSnips or UltiSnips
+lspconfig.html.setup{}
+
+-- YAML language server --
+lspconfig.yamlls.setup{}
+
