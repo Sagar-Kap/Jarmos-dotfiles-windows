1895650c16def5dc7f075e017eb5e39a55b0e212
Jarmos-san <somraj.mle@gmail.com>
2021-07-19 17:46:59 +0530

    :pencil2: Add some TODOs for future references

--- scripts/get_binaries.py
+++ scripts/get_binaries.py
@@ -5,4 +5,12 @@ from GitHub releases
 
 import urllib.requests as requests
 
+"""
+TODO: Download & move the binaries into their proper place i.e "c:/tools"
 
+For future reference:
+    - The endpoint to perform a GET request to is at:
+      https://api.github.com/repos/{org_name}/{repo_name}/releases/latest
+    - Documentation to refer to download the latest release asset is available
+      at: https://docs.github.com/en/rest/reference/repos#get-the-latest-release
+"""
