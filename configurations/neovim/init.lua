09ced09f9679e17d488a6a335b09ed7bfa13ab23
Jarmos-san <somraj.mle@gmail.com>
2021-07-19 18:16:50 +0530

    :truck: Backup config files to GitHub

--- configurations/neovim/init.lua
+++ configurations/neovim/init.lua
@@ -1,3 +1,19 @@
+[[ ----------------------------------------------------------------------------
+Yes, this is how I configure my Neovim environment! More info right below:
+
+First off, yes it might look confusing & weird but that's also the beauty of
+configuring Neovim with Lua. Compared to .vimrc/init.vim, you can modularize
+your init.vim pretty easily.
+
+So, you should write your Lua config files within a "./lua" directory & import
+those files into your "init.vim file. For example, if all your configurations
+were written in a "./lua/settings.lua" file, you should import it into your
+"./init.lua". Importing in Lua is pretty similar to pre-ES6 syntax.
+
+And don't forget to source your "init.lua" file after editing it.
+
+For more info, refer to https://github.com/nanotee/nvim-lua-guide
+---------------------------------------------------------------------------- ]]
 require('settings')
 require('plugins')
 require('lsp_config')
