67217e1380d76f3444144e7ed61d2af6ae7b3a3a
Jarmos-san <somraj.mle@gmail.com>
2021-07-20 18:20:04 +0530

    :truck: Backup config files to GitHub

--- configurations/neovim/lua/lsp_config.lua
+++ configurations/neovim/lua/lsp_config.lua
@@ -1,3 +1,10 @@
+-- Module for configuring Neovim's native LSP --
+-- The in-built LSP is configured through a plugin named: neovim/lspconfig
+-- But each Language Servers has to be configured seperately.
+-- For more information on how-to configure the individual language servers,
+-- refer to the CONFIG.md file in the plugin's repo which is also available at:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
+
 local lspconfig = require('lspconfig')
 
 -- Pyright Language Server for Python files
@@ -26,3 +33,13 @@ lspconfig.html.setup{}
 -- YAML language server --
 lspconfig.yamlls.setup{}
 
+-- EFM language server --
+-- TODO: Install the efm-language-server for working with linters & other
+-- related diagnostics concerns. More info are available at here:
+-- https://github.com/mattn/efm-langserver
+-- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#efm
+-- https://github.com/mantoni/eslint_d.js/
+-- https://phelipetls.github.io/posts/configuring-eslint-to-work-with-neovim-lsp/
+
+-- There's also another diagnostic plugin available below:
+-- https://github.com/iamcco/diagnostic-languageserver
