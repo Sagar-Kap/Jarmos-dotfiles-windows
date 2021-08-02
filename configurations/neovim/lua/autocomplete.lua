74a54438abf10f84a22df08b209a3316320a5281
Jarmos-san <somraj.mle@gmail.com>
2021-08-02 22:46:13 +0530

    :truck: Backup config files to GitHub

--- configurations/neovim/lua/autocomplete.lua
+++ configurations/neovim/lua/autocomplete.lua
@@ -1,10 +1,11 @@
 -- Module for CoC autocompletion plugin
 
 -- Install these global extensions if they've not been installed already
+-- TODO: Figure out how to install "coc-snippets"
 vim.g.coc_global_extensions = {
     'coc-json', 'coc-tsserver', 'coc-markdownlint', 'coc-pyright',
     'coc-diagnostic', 'coc-eslint', 'coc-powershell', 'coc-prettier',
-    'coc-snippets', 'coc-sumneko-lua', 'coc-toml', 'coc-yaml'
+    'coc-sumneko-lua', 'coc-toml', 'coc-yaml'
 }
 
 -- Configure directory to store CoC data & extensions
