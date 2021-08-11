5922e008237340dc295bacdcac387432f4cf926d
Jarmos-san <somraj.mle@gmail.com>
2021-08-11 15:00:24 +0530

    :truck: Backup config files to GitHub

--- configurations/neovim/ftplugin/markdown.lua
+++ configurations/neovim/ftplugin/markdown.lua
@@ -1,4 +1,4 @@
-vim.wo.wrap = true	              	-- Wrap Markdown files
+vim.wo.wrap = true			-- Wrap Markdown files
 vim.bo.textwidth = 80			-- Forces text files to move to next line after 80 characters
 vim.bo.expandtab = true			-- Inserts spaces when <Tab> is pressed
 vim.bo.softtabstop = 2			-- Inserts two spaces when <Tab> is pressed
