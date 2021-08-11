a7128153e220dd883afa87059812e1e16270558f
Jarmos-san <somraj.mle@gmail.com>
2021-08-11 15:39:58 +0530

    :truck: Backup config files to GitHub

--- configurations/neovim/ftplugin/lua.lua
+++ configurations/neovim/ftplugin/lua.lua
@@ -1,3 +1,4 @@
-vim.bo.expandtab = true			-- Inserts spaces when <Tab> is pressed
 vim.bo.softtabstop = 2			-- Inserts two spaces when <Tab> is pressed
+vim.bo.tabstop = 2                      -- A single press of <Tab> means two <Space>
 vim.bo.shiftwidth = 2			-- Not sure what is it supposed to do
+vim.bo.expandtab = true			-- Inserts spaces when <Tab> is pressed
--- configurations/neovim/ftplugin/markdown.lua
+++ configurations/neovim/ftplugin/markdown.lua
@@ -1,5 +1,6 @@
 vim.wo.wrap = true			-- Wrap Markdown files
 vim.bo.textwidth = 80			-- Forces text files to move to next line after 80 characters
-vim.bo.expandtab = true			-- Inserts spaces when <Tab> is pressed
 vim.bo.softtabstop = 2			-- Inserts two spaces when <Tab> is pressed
+vim.bo.tabstop = 2                      -- A single press of <Tab> means two <Space>
 vim.bo.shiftwidth = 2			-- Not sure what is it supposed to do
+vim.bo.expandtab = true			-- Inserts spaces when <Tab> is pressed
