0054913c36d5baceeb81cc5ea63a0609df4c935f
Jarmos-san <somraj.mle@gmail.com>
2021-08-13 13:11:11 +0530

    :Update bootstrap script to install Fira Code

--- scripts/Use-BootstrapScripts.ps1
+++ scripts/Use-BootstrapScripts.ps1
@@ -52,12 +52,14 @@ Invoke-WebRequest -Uri "https://get.scoop.sh" | Invoke-Expression
 Write-Host "Installing software not available through WinGet"
 
 # Scoop makes it easier to install some software which are generally distributed through binaries
-Invoke-Expression -Command "scoop install neovim less bat mingit hugo starship"
+Invoke-Expression -Command "scoop install neovim less bat hugo starship"
 
 # Add Scoop buckets to install additional software
 Invoke-Expression -Command "scoop bucket add nerd-fonts"
 
-# TODO: Install Fonts
+Write-Host "Installing Fira Code NF"
+
+Invoke-Expression -Command "scoop install firacode"
 
 # TODO: Install additional Python & NPM tools like "pipx", "yarn", & so on.
 # "pipx" will be used to download Python CLI tools like "poetry" & such.
