eb45e0c554742a100847f843b645bae484105673
Jarmos-san <somraj.mle@gmail.com>
2021-09-23 16:48:25 +0530

    :truck: Backup config files to GitHub

---
+++ configs/git-bash/.bashrc
@@ -0,0 +1,7 @@
+eval "$(starship init bash)"
+
+# Configures Starship to use the %TEMP% directory for caching
+export STARSHIP_CACHE="$HOME/AppData/Local/Temp"
+
+# Path to the Starship configuration file
+export STARSHIP_CONFIG="$HOME/.starship/starship.toml"
--- configs/windows-powershell/Microsoft.PowerShell_profile.ps1
+++ configs/windows-powershell/Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,6 @@ $ENV:STARSHIP_CACHE = Resolve-Path -Path $ENV:TEMP
 
 # Path to the Starship configuration file
 $ENV:STARSHIP_CONFIG = Resolve-Path -Path "$ENV:USERPROFILE\.starship\starship.toml"
+
+# Path to Windows Terminal configuration file
+$ENV:WTCONFIG = Resolve-Path -Path "$ENV:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
